978aa78095e53d2fe86a312e13805f7ad64e8754
MathieuM1 <mat.mercier45@gmail.com>
2016-12-02 17:30:07 +0100

    cleaning back layout

--- Corpo35/src/BackBundle/Controller/DefaultController.php
+++ Corpo35/src/BackBundle/Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('BackBundle:Default:index.html.twig');
+        return $this->render('BackBundle:Default:backIndex.html.twig');
     }
 }
