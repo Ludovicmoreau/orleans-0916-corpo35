6ceb18090e3bd846a35a4a9eaec34438e14c1766
MathieuM1 <mat.mercier45@gmail.com>
2016-12-02 09:40:47 +0100

    add User

--- Corpo35/src/BackBundle/Controller/JuryController.php
+++ Corpo35/src/BackBundle/Controller/JuryController.php
@@ -10,7 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;use Symfony\Component
 /**
  * Jury controller.
  *
- * @Route("/")
+ * @Route("/jury")
  */
 class JuryController extends Controller
 {
