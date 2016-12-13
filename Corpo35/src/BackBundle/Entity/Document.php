8c9917958706662f185fe33c31785cf8b23c108f
Messant <messant.d@gmail.com>
2016-12-13 15:50:22 +0100

    création des partenaires

--- Corpo35/src/BackBundle/Entity/Document.php
+++ Corpo35/src/BackBundle/Entity/Document.php
@@ -124,4 +124,6 @@ class Document
     {
         return $this->candidat;
     }
+
+
 }
---
+++ Corpo35/src/FrontBundle/Controller/ListAgendaLaboController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace FrontBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class ListAgendaLaboController extends Controller
+{
+    /**
+     * @Route("/listAgenda_labo", name="list_agenda_labo")
+     */
+    public function ShowAgendaLaboAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $listAgendas = $em->getRepository('BackBundle:Agenda_Labo')->findAll();
+        return $this->render('FrontBundle:Default:agenda_labo.html.twig', array(
+            'listAgendas' => $listAgendas
+        ));
+    }
+}
+
+
