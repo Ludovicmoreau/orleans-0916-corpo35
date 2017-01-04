f31bc79d829f0026c42fbbad2192391d5d326178
MathieuM1 <mat.mercier45@gmail.com>
2017-01-04 14:29:47 +0100

    create crud vote

---
+++ Corpo35/src/BackBundle/Form/VoteType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace BackBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class VoteType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('note')
+            ->add('candidat')
+            ->add('jury')        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BackBundle\Entity\Vote'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'backbundle_vote';
+    }
+
+
+}
