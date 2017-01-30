1f32710b29e07ae19d4ab145995328cf807e6a0e
Messant <messant.d@gmail.com>
2017-01-30 20:13:58 +0100

    pdf dynamique

--- Corpo35/src/FrontBundle/Controller/ReglementController.php
+++ Corpo35/src/FrontBundle/Controller/ReglementController.php
@@ -2,8 +2,12 @@
 
 namespace FrontBundle\Controller;
 
+use BackBundle\Entity\Reglement;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class ReglementController extends Controller
 {
@@ -17,5 +21,31 @@ class ReglementController extends Controller
         return $this->render('FrontBundle:Default:reglement.html.twig', [
             'reglement' => $reglement,
         ]);
+    }
+
+
+    /**
+     * Export to PDF
+     *
+     * @Route("/reglement/pdf", name="reglement_pdf")
+     */
+    public function pdfAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $reglement = $em->getRepository('BackBundle:Reglement')->findAll();
+
+        $html = $this->renderView('FrontBundle:Default:reglement_pdf.html.twig', array(
+            'reglement' => $reglement
+        ));
+
+        return new Response(
+            $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
+            200,
+            array(
+                'Content-Type' => 'application/pdf',
+                'Content-Disposition' => 'attachment; filename="file.pdf"'
+            )
+        );
+
     }
 }
