51c4dc15e66ff138370acf43854399e6c087b198
Anne-Laure Fontaine <anne.lfontaine@gmail.com>
2017-01-31 10:12:31 +0100

    reprise des formulaires + ajout des nb

--- Corpo35/src/BackBundle/Form/RpresseType.php
+++ Corpo35/src/BackBundle/Form/RpresseType.php
@@ -3,7 +3,9 @@
 namespace BackBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +16,19 @@ class RpresseType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('description')
-                ->add('source')
-                ->add('date')
+        $builder->add('description', TextType::class, [
+                    'attr' => [
+                        'placeholder' => 'Example : type de parution (magazine, presse etc.)'
+                    ]
+                ])
+                ->add('source', TextType::class, [
+                    'attr' => [
+                        'placeholder' => 'Si lien internet, ne pas oublier http ou https://www.example.fr'
+                    ]
+                ])
+                ->add('date', DateType::class)
                 ->add('document', FileType::class, array(
-                'data_class' => null
+                'data_class' => null,
                 ))
         ;
     }
